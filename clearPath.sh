e919c07ccfd505b25d5ecd51907252708043e8c4
VaporFoxLash <ldonald298@gmail.com>
2019-11-08 01:02:06 +0500

    Display help message

--- clearPath.sh
+++ clearPath.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-bool=false
 clr=":"
 IFS=':'
-if [ "$1" != "--help" ] && [ "$1" != "-h" ]; then
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+	echo "Run clearPath.sh or enter any directory to clear path"
+else
 	for directory in $PATH; do
 		if [[ ! -d "$directory" ]]
     	then
         	continue
     	fi
-		for file in $directory/*; do
+		for file in "$directory"/*
+		do
 			if [[ -f "$file" && -x "$file" ]]; then
             	#echo "Executable File: " $file
 				if [[ ! $clr = *:$directory:* ]]; then   
                 	clr="$clr$directory:"
                 	break
             	fi	
-    		elif [[ -z ${res:1} ]]; then
+    		elif [[ -z ${clr:1} ]]
+    		then
     			echo "Path does not contain executable files"
 			else   
     			echo "${clr:1:-1}"
@@ -24,4 +27,3 @@ if [ "$1" != "--help" ] && [ "$1" != "-h" ]; then
 		done
 	done
 fi
-#PATH=$(echo $PATH | awk -v RS=: -v ORS=: '!($0 in a) {a[$0]; print}')
